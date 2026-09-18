d1791d0990031c45816400005b099f804adaf037
azimovazizbek838-hash <azimovazizbek838@gmail.com>
2026-09-18 17:05:05 +0500

    Add dummy menu data for burgers and pizzas

---
+++ menuData.js
@@ -0,0 +1,18 @@
+export const dummyMenu = [
+  {
+    id: 1,
+    name: "Classic Burger",
+    category: "Burgerlar",
+    price: 45000,
+    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+    description: "Sersuv mol go'shti, pishloq va maxsus sous"
+  },
+  {
+    id: 2,
+    name: "Pepperoni Pizza",
+    category: "Pizzalar",
+    price: 75000,
+    image: "https://images.unsplash.com/photo-1628840042765-356cda07504e",
+    description: "Klassik pepperoni kolbasasi va motsarella pishlog'i"
+  }
+];
